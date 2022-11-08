f3e6738f9fb53eb399b7bda8b0aa8cdf884c2506
Zaid K. Al Qassar <wgs.xperia@gmail.com>
2022-11-09 00:19:42 +0300

    Add: Adding toolbar items and styling it to match the concept

--- app/src/main/java/com/muzz/muzzdemo/MainActivity.java
+++ app/src/main/java/com/muzz/muzzdemo/MainActivity.java
@@ -1,14 +1,39 @@
 package com.muzz.muzzdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tool_bar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.nav_options) {
+            Toast.makeText(this, "Clicked More Options..", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
