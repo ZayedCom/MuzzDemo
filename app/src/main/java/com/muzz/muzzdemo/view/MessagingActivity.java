474f7fb765d51d8932e5a59e857c19caef2a4e49
Zaid K. Al Qassar <wgs.xperia@gmail.com>
2022-11-09 17:56:45 +0300

    Add: Adding viewModel to the view and the associated activity interface

--- app/src/main/java/com/muzz/muzzdemo/view/MessagingActivity.java
+++ app/src/main/java/com/muzz/muzzdemo/view/MessagingActivity.java
@@ -3,26 +3,97 @@ package com.muzz.muzzdemo.view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.muzz.muzzdemo.R;
+import com.muzz.muzzdemo.databinding.ActivityMessagingBinding;
+import com.muzz.muzzdemo.utility.Database;
+import com.muzz.muzzdemo.viewmodel.MessagingViewModel;
+
+import java.util.Objects;
 
 public class MessagingActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
+    public static final String myPreferences = "myExchangeRates";
+
+    public static SharedPreferences sharedPreferences;
+
+    public Database database;
+
+    public SQLiteDatabase dbSQL;
+
+    private static Bundle bundleRecyclerViewState;
+
+    private MessagingViewModel messagingViewModel;
+
+    private ActivityMessagingBinding binding;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messaging);
 
-        toolbar = findViewById(R.id.toolbar);
+        database = new Database(MessagingActivity.this);
+        dbSQL = database.getWritableDatabase();
+        database.close();
+
+        sharedPreferences = getSharedPreferences(myPreferences, Context.MODE_PRIVATE);
+
+        messagingViewModel = new ViewModelProvider(this).get(MessagingViewModel.class);
+
+        binding = DataBindingUtil.setContentView(MessagingActivity.this, R.layout.activity_messaging);
+
+        binding.setLifecycleOwner(this);
+
+        binding.setMessagingViewModel(messagingViewModel);
+
+        binding = ActivityMessagingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        toolbar = binding.toolbar;
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // save RecyclerView state when changing UI orientation
+        /*
+        bundleRecyclerViewState = new Bundle();
+        if (Objects.requireNonNull(binding.messagesRecyclerView.getLayoutManager()).getChildAt(0) != null) {
+            Parcelable listState = binding.messagesRecyclerView.getLayoutManager().onSaveInstanceState();
+            bundleRecyclerViewState.putParcelable(myPreferences, listState);
+        */
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // restore RecyclerView state when changing UI orientation
+        /*
+        if (!database.getDBMessages().isEmpty()) {
+            if (Objects.requireNonNull(binding.messagesRecyclerView.getLayoutManager()).getChildAt(0) != null) {
+                Parcelable listState = bundleRecyclerViewState.getParcelable(myPreferences);
+                binding.messagesRecyclerView.getLayoutManager().onRestoreInstanceState(listState);
+            }
+        }
+         */
     }
 
     @Override
